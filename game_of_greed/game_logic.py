5343996e1d04fb5040103010c79c20c2785463d1
Mason Aviles <mce.aviles@gmail.com>
2021-05-24 11:45:18 -0700

    initial set up

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,28 @@
+import re, random
+
+class GameLogic():
+
+    def __init__(self, value):
+        self.value = value
+
+    def calculate_score(): # static method
+        pass
+
+    def roll_dice(): # static method
+        pass
+
+
+
+class Banker():
+
+    def __init__(self, value):
+        self.value = value
+
+    def shelf(): # instance method
+        pass
+
+    def bank(): # instance method
+        pass
+
+    def clear_shelf(): # instance method
+        pass
