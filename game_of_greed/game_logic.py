c15cd7bb793f71ee0ed60a9d52a9dfe2172043d2
Mason Aviles <mce.aviles@gmail.com>
2021-05-24 16:51:18 -0700

    done with dice roll

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -2,15 +2,51 @@ import re, random
 
 class GameLogic():
 
-    def __init__(self, value):
-        self.value = value
+    dice_roll = random.randint(1, 6)
 
-    def calculate_score(): # static method
+    @staticmethod
+    def calculate_score(self): # static method
         pass
 
-    def roll_dice(): # static method
-        pass
-
+    @staticmethod
+    def roll_dice(num_of_dice): # static method
+        if num_of_dice == 1:
+            values = []
+            dice_roll = random.randint(1, 6)
+            values.append(dice_roll)
+            return tuple(values)
+        elif num_of_dice == 2:
+            values = []
+            for num in range(0, num_of_dice):
+                dice_roll = random.randint(1, 6)
+                values.append(dice_roll)
+            return tuple(values)
+        elif num_of_dice == 3:
+            values = []
+            for num in range(0, num_of_dice):
+                dice_roll = random.randint(1, 6)
+                values.append(dice_roll)
+            return tuple(values)
+        elif num_of_dice == 4:
+            values = []
+            for num in range(0, num_of_dice):
+                dice_roll = random.randint(1, 6)
+                values.append(dice_roll)
+            return tuple(values)
+        elif num_of_dice == 5:
+            values = []
+            for num in range(0, num_of_dice):
+                dice_roll = random.randint(1, 6)
+                values.append(dice_roll)
+            return tuple(values)
+        elif num_of_dice == 6:
+            values = []
+            for num in range(0, num_of_dice):
+                dice_roll = random.randint(1, 6)
+                values.append(dice_roll)
+            return tuple(values)
+        else:
+            return -1
 
 
 class Banker():
@@ -18,10 +54,10 @@ class Banker():
     def __init__(self, value):
         self.value = value
 
-    def shelf(): # instance method
+    def shelf(self): # instance method
         pass
 
-    def bank(): # instance method
+    def bank(self): # instance method
         pass
 
     def clear_shelf(): # instance method
