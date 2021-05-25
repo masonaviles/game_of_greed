7c48d28877177471972ac4c138d3de966101b829
Mason Aviles <mce.aviles@gmail.com>
2021-05-25 14:51:57 -0700

    saving

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -9,44 +9,6 @@ class GameLogic():
             values.append(random.randint(1,6))
         return tuple(values)
 
-        # if num_of_dice == 1:
-        #     values = []
-        #     dice_roll = random.randint(1, 6)
-        #     values.append(dice_roll)
-        #     return tuple(values)
-        # elif num_of_dice == 2:
-        #     values = []
-        #     for num in range(0, num_of_dice):
-        #         dice_roll = random.randint(1, 6)
-        #         values.append(dice_roll)
-        #     return tuple(values)
-        # elif num_of_dice == 3:
-        #     values = []
-        #     for num in range(0, num_of_dice):
-        #         dice_roll = random.randint(1, 6)
-        #         values.append(dice_roll)
-        #     return tuple(values)
-        # elif num_of_dice == 4:
-        #     values = []
-        #     for num in range(0, num_of_dice):
-        #         dice_roll = random.randint(1, 6)
-        #         values.append(dice_roll)
-        #     return tuple(values)
-        # elif num_of_dice == 5:
-        #     values = []
-        #     for num in range(0, num_of_dice):
-        #         dice_roll = random.randint(1, 6)
-        #         values.append(dice_roll)
-        #     return tuple(values)
-        # elif num_of_dice == 6:
-        #     values = []
-        #     for num in range(0, num_of_dice):
-        #         dice_roll = random.randint(1, 6)
-        #         values.append(dice_roll)
-        #     return tuple(values)
-        # else:
-        #     return -1
-
     @staticmethod
     def calculate_score(tuple_of_dice_roll): # static method
         score = 0
@@ -55,7 +17,7 @@ class GameLogic():
         # most_common(): Return a list of the n most common elements and their counts from the most common to the least. If n is omitted or None, returns all elements in the counter.
         counter = collections.Counter(tuple_of_dice_roll).most_common()
 
-         #Empty
+        # Empty
         if len(dice_roll) == 0:
             score += 0
             return score
@@ -92,36 +54,6 @@ class GameLogic():
             else:
                 score += 0
                 return score
-
-
-        # if tuple_of_dice_roll == (5,):
-        #     return 50
-        # elif tuple_of_dice_roll == (1, 1):
-        #     return 200
-        # elif tuple_of_dice_roll == (1, 5):
-        #     return 150
-        # elif tuple_of_dice_roll == (2,):
-        #     return 0
-        # elif tuple_of_dice_roll == (5, 5, 5, 2, 2, 3):
-        #     return 500
-        # elif tuple_of_dice_roll == (1,) or tuple_of_dice_roll == (5,5):
-        #     return 100
-        # elif tuple_of_dice_roll == (1, 1, 1, 2, 3, 4):
-        #     return 1000
-        # elif tuple_of_dice_roll == (1, 1, 1, 5):
-        #     return 1050
-        # elif tuple_of_dice_roll == (1, 6, 3, 2, 5, 4):
-        #     return 1500
-        # elif tuple_of_dice_roll == (2,2,2):
-        #     return 200
-        # elif tuple_of_dice_roll == (2,2,2,2):
-        #     return 400
-        # elif tuple_of_dice_roll == (2,2,2,2,2):
-        #     return 600
-        # elif tuple_of_dice_roll == (2,2,2,2,2,2):
-        #     return 800
-        # elif tuple_of_dice_roll == (1,1,1,1,1,1):
-        #     return 4000
         
 
 
